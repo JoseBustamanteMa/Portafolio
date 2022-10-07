3fc2398e1ca45b0de7d04ee7ad5e8e04c900e84d
trejo <jere.aranda@alumnos.duoc.cl>
2022-10-06 22:05:38 -0300

    Implementacion de REST API

---
+++ API-REST-con-Node-js-y-MySQL-master/routes.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const routes = express.Router()
+
+routes.get('/', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('SELECT * FROM comuna', (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.json(rows)
+        })
+    })
+})
+
+routes.post('/', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('INSERT INTO comuna set ?', [req.body], (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('Comuna añadida')
+        })
+    })
+})
+
+routes.delete('/:id', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('DELETE FROM comuna WHERE id = ?', [req.params.id], (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('Comuna eliminada')
+        })
+    })
+})
+
+routes.put('/:id', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('UPDATE comuna set ? WHERE id = ?', [req.body, req.params.id], (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('comuna cambiada')
+        })
+    })
+})
+
+module.exports = routes
